5f97ecaaf8d9185f323684b2adb06c4353f38903
Michael Aguirre <michaelaguirre3.30.20@gmail.com>
2020-11-20 11:20:20 -0500

    initial commit

---
+++ Week1Challenge/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Week1Challenge")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Week1Challenge")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7d66d98e-9899-4517-a037-76986e60d4f7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Week1Challenge/UnitTest1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Week1Challenge
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void NameAndAge()
+        {
+            string firstName = "Mike";
+            string lastName = "Aguirre";
+            int age = 29;
+            Console.WriteLine(age + 7);
+            Console.WriteLine(age - 7);
+            Console.WriteLine(age * 7);
+            Console.WriteLine(age / 7);
+            Console.WriteLine(age % 7);
+        }
+        [TestMethod]
+        public void Array()
+        {
+            string[] movies = { "Batman", "Little Nicky", "Beetlejuice", "Brothers" };
+
+        }
+        [TestMethod]
+        public void ListOfDates()
+        {
+            List<DateTime> listOfDates = new List<DateTime>();
+            DateTime today = DateTime.Now;
+            listOfDates.Add(today);
+            listOfDates.Add(new DateTime(2020, 11, 26));
+            listOfDates.Add(new DateTime(2020, 12, 25));
+            listOfDates.Add(new DateTime(2020, 12, 31));
+            listOfDates.Add(new DateTime(2021, 1, 1));
+        }
+        [TestMethod]
+        public void HowMuchSleepDidYouGet()
+        {
+            Console.WriteLine("How many hours of sleep did you get?");
+            int hours = 4;
+            if(hours >=10)
+            {
+                Console.WriteLine("Woww, that's alot of sleep!");
+            }
+            else if(hours >= 8 && hours < 10)
+            {
+                Console.WriteLine("You should be well rested");
+            }
+            else if(hours > 4 && hours < 8)
+            {
+                Console.WriteLine("Bummer...");
+            }
+            else
+            {
+                Console.WriteLine("Oh man, get some sleep!");
+            }
+        }
+        [TestMethod]
+        public void HowHasYourDayBeen()
+        {
+            string userDay = "Okay";
+            switch(userDay)
+            {
+                case "Great":
+                    Console.WriteLine("That's awesome!");
+                    break;
+                case "Good":
+                    Console.WriteLine("Everyday above ground is a good day!");
+                    break;
+                case "Okay":
+                    Console.WriteLine("I hope it gets better for you");
+                    break;
+                case "Bad":
+                    Console.WriteLine("The grass is always greener on the other side");
+                    break;
+                case ":(":
+                    Console.WriteLine("I guess this is us now");
+                    break;
+                default:
+                    Console.WriteLine("Please input a response");
+                    break;
+            }
+        }
+        [TestMethod]
+        public void Supercalifragilisticexpialidocious()
+        {
+            string name = "Supercalifragilisticexpialidocious";
+            foreach(char letter in name)
+            {
+                if(letter == 'i')
+                {
+                    Console.WriteLine(letter);
+                }
+                else
+                {
+                    Console.WriteLine("not an i");
+                }    
+            }
+        }
+    }
+}
